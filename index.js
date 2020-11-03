d1cce3d496a4fc40212eeb04c5a5de9f23d0130b
wss-git <wssgryx@163.com>
2020-11-03 13:42:57 +0800

    logs 功能

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ class FcComponent extends Component {
   constructor() {
     super();
   }
+
+  async logs (inputs) {
+    const fcLogs = await this.load('fc-logs-alibaba-component', 'Component');
+    await fcLogs.logs(inputs);
+  }
   
-  async metrics(inputs) {
+  async metrics (inputs) {
     const fcMetrics = await this.load('fc-metrics-alibaba-component', 'Component');
     await fcMetrics.metrics(inputs);
   }
