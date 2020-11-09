1940bd3bfa42751f7d2de476626db201d1839c5f
wss-git <wssgryx@163.com>
2020-11-09 11:03:55 +0800

    check log

--- utils/check.js
+++ utils/check.js
@@ -25,23 +25,21 @@ const checkFunctionName = functionName => {
   }
 }
 
-const checkLogConfig = logConfig => {
+function checkLogsCommands (region, serviceName, functionName, logConfig) {
+  checkRegion(region);
+  checkServiceName(serviceName);
+  checkFunctionName(functionName);
+
   if (!logConfig) {
     new ServerlessError({
       code: 'LogNotFount',
       message: 'Log config is empty.'
     });
   }
-}
 
-function checkLogsCommands (region, serviceName, functionName, logConfig) {
-  checkRegion(region);
-  checkServiceName(serviceName);
-  checkFunctionName(functionName);
-  checkLogConfig(logConfig);
-
-  const isObj = typeof logConfig !== 'string' && !(logConfig.Project && logConfig.LogStore);
-  if (!isConfigAuto(logConfig) || isObj) {
+  const checkAuto = typeof logConfig === 'string' && !isConfigAuto(logConfig);
+  const checkObj = typeof logConfig !== 'string' && !(logConfig.Project && logConfig.LogStore);
+  if (checkAuto || checkObj) {
     new ServerlessError({
       code: 'LogConfigError',
       message: 'Missing Log definition in template.yml.\nRefer to https://github.com/Serverless-Devs-Awesome/fc-alibaba-component#log'
